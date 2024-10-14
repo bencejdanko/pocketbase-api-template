e3812ff14da242d86c0b0ecb50794555336f9026
bencejdanko <bencejdanko@gmail.com>
2024-10-14 00:53:20 -0700

    pb app

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 git remote -v to get remotes
 
 git push dokku-lan main
+
+add ssl with letencrypt plugin: dokku letsencrypt:cron-job --add
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.GET("/hello/:name", func(c echo.Context) error {
+			name := c.PathParam("name")
+
+			return c.JSON(http.StatusOK, map[string]string{"message": "Hello " + name})
+		} /* optional middlewares */)
+
+		return nil
+	})
+}
